3c5c34d0aa676141a75274f617062cf33c0b2ff3
Piotr Bieszczad <biecho88@gmail.com>
2024-03-24 21:30:49 +0000

    Changed model name and customized admin to display more informations on the smoothies app panel

--- smoothies/admin.py
+++ smoothies/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Smoothie, Category
 
 # Register your models here.
-admin.site.register(Smoothie)
-admin.site.register(Category)
+
+class SmoothieAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Smoothie, SmoothieAdmin)
+admin.site.register(Category, CategoryAdmin)
 
